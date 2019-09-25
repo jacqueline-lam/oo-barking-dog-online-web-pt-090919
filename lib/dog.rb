3d20b26a085f8621d33884e37efdf21c9f37ba67
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-09-25 22:29:08 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,17 @@
 # Create a Dog class
 # Every instance of a Dog should have a name and be able to bark
 
-class Dog
+class Dog # define a class - Dog
+  
+  #setter method
+  def name=(dogs_name)
+    @this_dog_name = dogs_name
+  end
+  
+  #getter method
+  def name
+    @this_dogs_name
+  end
+
 end 
+
